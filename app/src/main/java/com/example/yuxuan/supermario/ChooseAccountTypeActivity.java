cb4f34aa34fd8bda3921d6576a34e8d83194f676
dengmingwei <mingweideng@gmail.com>
2018-10-21 16:44:08 -0400

    Intent
    Intent for each Button

--- app/src/main/java/com/example/yuxuan/supermario/ChooseAccountTypeActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/ChooseAccountTypeActivity.java
@@ -1,13 +1,47 @@
 package com.example.yuxuan.supermario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseAccountTypeActivity extends AppCompatActivity {
-
+    private Button button1;
+    private Button button2;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_account_type);
+
+        button1=findViewById(R.id.createAdminTypeBtn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccountButton();
+            }
+        });
+        button2=findViewById(R.id.createProviderTypeBtn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccountButton();
+            }
+        });
+        button3=findViewById(R.id.createOwnerTypeBtn);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccountButton();
+            }
+        });
     }
+    public void CreateAccountButton(){
+        Intent intent=new Intent(getApplicationContext(),CreateAccountActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+
+
 }
--- app/src/main/java/com/example/yuxuan/supermario/MainActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.yuxuan.supermario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.createAccBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OnAccountTypeButton();
+            }
+        });
+
+    }
+    public void OnAccountTypeButton(){
+        Intent intent=new Intent(getApplicationContext(),ChooseAccountTypeActivity.class);
+        startActivityForResult(intent,0);
     }
 }
