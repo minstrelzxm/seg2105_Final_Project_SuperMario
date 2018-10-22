f7a9f98a40882add30662e3c039656bbabe7b5c3
Xumou Zhang <jzhan279@uottawa.ca>
2018-10-21 20:13:56 -0400

    import database
    import database to CreateAccountActivity

--- app/src/main/java/com/example/yuxuan/supermario/CreateAccountActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/CreateAccountActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateAccountActivity extends AppCompatActivity {
     private Button BackMainButton;
