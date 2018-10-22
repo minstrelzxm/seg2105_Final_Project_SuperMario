b31f358e9becb1a467cea843f5876311b1c4c359
Xumou Zhang <jzhan279@uottawa.ca>
2018-10-21 20:36:03 -0400

    Update app/src/main/java/com/example/yuxuan/supermario/CreateAccountActivity.java
    firebase parts editing

--- app/src/main/java/com/example/yuxuan/supermario/CreateAccountActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/CreateAccountActivity.java
@@ -6,18 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.ListView;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CreateAccountActivity extends AppCompatActivity {
     private Button BackMainButton;
     @Override
+    DatabaseReference databaseProducts;
+
     protected void onCreate(Bundle savedInstanceState) {
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
