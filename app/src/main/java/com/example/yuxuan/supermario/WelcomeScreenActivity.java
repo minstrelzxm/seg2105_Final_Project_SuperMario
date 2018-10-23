4d56766936f4ed50809c5986a3d9a7c0d225e032
MaiCyclone <jyx940709@gmail.com>
2018-10-23 16:26:04 -0400

    Update welcome screen
    still need to set the account type and its username

--- app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
@@ -2,12 +2,27 @@ package com.example.yuxuan.supermario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
+
+    TextView welcomeAccName;
+    TextView welcomeAccType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        //TODO: The welcome screen should show the type of account and its's user name;
+        welcomeAccName = findViewById(R.id.welcomeTextViewUserName);
+        welcomeAccType = findViewById(R.id.welcomeTextViewUserRole);
+
     }
+
+
+
+
+
 }
