629d9d3f46ed49a362cc808491939ab72b86a4fb
Xumou Zhang <jzhan279@uottawa.ca>
2018-10-22 20:53:53 -0400

    update choose account
    add current type for this file

--- app/src/main/java/com/example/yuxuan/supermario/ChooseAccountTypeActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/ChooseAccountTypeActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//import MyAccountType;
+
+import com.google.android.gms.common.internal.AccountType;
 
 public class ChooseAccountTypeActivity extends AppCompatActivity {
     private Button Adminbutton;
     private Button Providerbutton;
     private Button Ownerbutton;
+    MyAccountType currentType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,7 @@ public class ChooseAccountTypeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Account adminAccount = new Account(" "," ",MyAccountType.administrator);
+               currentType = MyAccountType.administrator;
                 CreateAccountButton();
 
             }
@@ -28,6 +33,8 @@ public class ChooseAccountTypeActivity extends AppCompatActivity {
         Providerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Account providerAccount = new Account(" "," ",MyAccountType.serviceProviders);
+                currentType = MyAccountType.serviceProviders;
                 CreateAccountButton();
             }
         });
@@ -35,6 +42,8 @@ public class ChooseAccountTypeActivity extends AppCompatActivity {
         Ownerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Account ownerAccount = new Account(" "," ",MyAccountType.homeOwners);
+                currentType = MyAccountType.homeOwners;
                 CreateAccountButton();
             }
         });
