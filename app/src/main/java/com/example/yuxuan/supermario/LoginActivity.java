cf992746f6ee5cd3c9aa44ea5db42daf49738c12
dengmingwei <mingweideng@gmail.com>
2018-10-24 10:26:34 -0400

    Validate Eamil for login

--- app/src/main/java/com/example/yuxuan/supermario/LoginActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/LoginActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private Button LoginButton;
+    EditText mail, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +23,35 @@ public class LoginActivity extends AppCompatActivity {
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OnLoginButton();
+                String validate=
+
+                        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@" +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                        "(" +
+                        "\\." +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                        ")+";
+
+                String email=mail.getText().toString();
+                Matcher matcher= Pattern.compile(validate).matcher(email);
+
+                if(matcher.matches()){
+                    Toast.makeText(getApplicationContext(),"Valid Email",Toast.LENGTH_LONG).show();
+                    OnLoginButton();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invaild Email",Toast.LENGTH_LONG).show();
+                }
+                if (password.getText().toString().equals("")){
+                    password.setError("Enter correct password");
+                }
+
             }
         });
+
+        mail=(EditText)findViewById(R.id.loginTextUsernameInput);
+        password=(EditText)findViewById(R.id.loginTextUserPasswordInput);
 
     }
     public void OnLoginButton(){
