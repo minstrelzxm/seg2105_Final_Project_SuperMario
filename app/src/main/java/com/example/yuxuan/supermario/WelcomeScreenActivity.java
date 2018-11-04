2f99a57b456538ea75a190e2b8ff1b74a6958400
MaiCyclone <jyx940709@gmail.com>
2018-11-03 22:39:04 -0400

    Add conditions at welcome screen
    In the welcome screen admin can only press AdminBtn, and same for serviceProvider and Home Owner.

--- app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
@@ -3,29 +3,52 @@ package com.example.yuxuan.supermario;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
 
-    String username;
-    String password;
+    String userName;
+    String roleName;
+    Button adminBtn = (Button)findViewById(R.id.welcomeAdminButton);
+    Button providerBtn = (Button)findViewById(R.id.welcomeProviderButton);
+    Button ownerBtn = (Button)findViewById(R.id.welcomeOwnerButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
         //TODO: set userRole and userName to its corresponding type
-        TextView err = (TextView)findViewById(R.id.welcomeTextViewUserName);
-        TextView kkk = (TextView)findViewById(R.id.welcomeTextViewUserRole);
+        TextView userNameTextView = (TextView)findViewById(R.id.welcomeTextViewUserName);
+        TextView userRoleTextView = (TextView)findViewById(R.id.welcomeTextViewUserRole);
         Intent intent = getIntent();
-        username = intent.getStringExtra("username");
-        password = intent.getStringExtra("accounttype");
-        kkk.setText(password);
-        err.setText(username);
+        userName = intent.getStringExtra("username");
+        roleName = intent.getStringExtra("accounttype");
+        userNameTextView.setText(userName);
+        userRoleTextView.setText(roleName);
+
+        if(roleName =="administrator"){
+            //roleName from MyAccountType
+            providerBtn.setEnabled(false);
+            ownerBtn.setEnabled(false);
+
+        }else if(roleName =="serviceProviders"){
+            //roleName from MyAccountType
+            adminBtn.setEnabled(false);
+            ownerBtn.setEnabled(false);
+
+        }else if(roleName =="homeOwners"){
+            //roleName from MyAccountType
+            providerBtn.setEnabled(false);
+            adminBtn.setEnabled(false);
+
+        }
+
     }
 
     //TODO: Admin to activity_admin_page; disable other buttons
     //TODO: provider to provider page;
     //TODO: Owner to owner page;
+
 }
