e7cd9d55327e8aca5e16a78e2cc3d06e9e32e6fb
dengmingwei <mingweideng@gmail.com>
2018-11-04 21:39:18 -0500

    Welcome Intent
    Welcome screen intent

--- app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/WelcomeScreenActivity.java
@@ -3,6 +3,7 @@ package com.example.yuxuan.supermario;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,32 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        adminBtn=findViewById(R.id.welcomeAdminButton);
+        // jump to AdminPageActivity
+        adminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adminBtn();
+            }
+        });
+        providerBtn=findViewById(R.id.welcomeProviderButton);
+        // jump to ChooseAccountTypeActivity
+        providerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                providerBtn();
+            }
+        });
+
+        ownerBtn=findViewById(R.id.welcomeOwnerButton);
+        // jump to ChooseAccountTypeActivity
+        ownerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                providerBtn();
+            }
+        });
 
         //TODO: set userRole and userName to its corresponding type
         TextView userNameTextView = (TextView)findViewById(R.id.welcomeTextViewUserName);
@@ -48,7 +75,29 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     }
 
     //TODO: Admin to activity_admin_page; disable other buttons
+
+    //Using intent to jump to AdminPageActivity
+    public void adminBtn() {
+        Intent intent = new Intent(getApplicationContext(), AdminPageActivity.class);
+        startActivityForResult(intent, 0);
+    }
     //TODO: provider to provider page;
+
+    //Using intent to jump to create_provider_account
+    public void providerBtn() {
+        Intent intent = new Intent(getApplicationContext(), create_provider_account.class);
+        startActivityForResult(intent, 0);
+    }
     //TODO: Owner to owner page;
 
+    //Using intent to jump to create_owner_account
+    public void OwnerBtn() {
+        Intent intent = new Intent(getApplicationContext(), create_owner_account.class);
+        startActivityForResult(intent, 0);
+    }
+
+
+
+
 }
+
