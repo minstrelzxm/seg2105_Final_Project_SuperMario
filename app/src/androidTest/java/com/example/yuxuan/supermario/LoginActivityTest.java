2cfa3f2b9d3a12621bc77b522aa5c7cb88976fd1
Xumou Zhang <jzhan279@uottawa.ca>
2018-11-10 16:11:42 -0500

    unit test added
    unit test added:
    CreateAccountActivityTest
    LoginActivityTest

---
+++ app/src/androidTest/java/com/example/yuxuan/supermario/LoginActivityTest.java
@@ -0,0 +1,49 @@
+package com.example.yuxuan.supermario;
+
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule= new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    private LoginActivity mActivity=null;
+    private TextView text;
+
+    @Before
+    public void setUp() throws Exception{
+        mActivity=mActivityTestRule.getActivity();
+    }
+
+
+    @Test
+    @UiThreadTest
+    public void checkEmail() throws Exception{
+        assertNotNull(mActivity.findViewById(R.id.textView6));
+        text = mActivity.findViewById(R.id.loginTextUsernameInput);
+        text.setText("mingweideng@gmail.com");
+        String name=text.getText().toString();
+        assertNotEquals("username",name);
+    }
+
+    @Test
+    @UiThreadTest
+    public void checkPassword() throws Exception{
+        assertNotNull(mActivity.findViewById(R.id.textView8));
+        text = mActivity.findViewById(R.id.loginTextUserPasswordInput);
+        text.setText("123456");
+        String name=text.getText().toString();
+        assertNotEquals("password",name);
+    }
+
+}
