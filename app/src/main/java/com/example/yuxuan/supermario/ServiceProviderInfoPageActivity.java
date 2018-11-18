1d6fe4802e9f55292dda28ac597d993d56a2cae4
Xumou Zhang <jzhan279@uottawa.ca>
2018-11-17 21:45:04 -0500

    add ServiceProiderInfo
    add ServiceProiderInfo class, add onCreate(), addInfo() to ServiceProiderInfoActivity

--- app/src/main/java/com/example/yuxuan/supermario/ServiceProviderInfoPageActivity.java
+++ app/src/main/java/com/example/yuxuan/supermario/ServiceProviderInfoPageActivity.java
@@ -32,25 +32,80 @@ import java.util.regex.Pattern;
 
 public class ServiceProviderInfoPageActivity extends AppCompatActivity {
 
-    @Override
-    private EditText address;
-    private EditText phoneNum;
-    private EditText nameOfCompany;
-    private EditText generalInfo;
-    private EditText licensed;
+
+    private EditText editAddress;
+    private EditText editPhoneNum;
+    private EditText editNameOfCompany;
+    private EditText editGeneralInfo;
+    private EditText editLicensed;
+    private Button saveButton;
+
 
     //FirebaseAuth mAuth;
-    FirebaseDatabase mDatabase;
-
+    //FirebaseDatabase mDatabase;
+    DatabaseReference databaseInfo;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_info_page);
-        address = (EditText) findViewById(R.id.EditAddress);
-        phoneNum = (EditText) findViewById(R.id.EditPhoneNum);
-        nameOfCompany = (EditText) findViewById(R.id.EditNameOfCompany);
-        generalInfo = (EditText) findViewById(R.id.EditGeneralInfo);
-        
+        editAddress = (EditText) findViewById(R.id.EditAddress);
+        editPhoneNum = (EditText) findViewById(R.id.EditPhoneNum);
+        editNameOfCompany = (EditText) findViewById(R.id.EditNameOfCompany);
+        editGeneralInfo = (EditText) findViewById(R.id.EditGeneralInfo);
+        editLicensed = (EditText) findViewById(R.id.BooleanLicensed);
+
+        saveButton=(Button)findViewById(R.id.SaveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addInfo();
+            }
+        });
+
     }
 
     //TODO: add this service provider to database
+    public void serviceProviderJump(){
+        Intent intent = new Intent(getApplicationContext(),ServiceProviderMainPage.class);
+        startActivityForResult(intent,0);
+    }
+
+    private void addInfo() {
+        //getting the values to save
+        String address = editAddress.getText().toString().trim();
+        String phoneNum = editPhoneNum.getText().toString().trim();
+        String nameOfCompany = editNameOfCompany.getText().toString().trim();
+        String generalInfo = editGeneralInfo.getText().toString().trim();
+        String licensed = editLicensed.getText().toString().trim();
+
+        //checking if the value is provided
+        if (!TextUtils.isEmpty(address)||!TextUtils.isEmpty(phoneNum)||!TextUtils.isEmpty(nameOfCompany)||
+                !TextUtils.isEmpty(generalInfo)||!TextUtils.isEmpty(licensed)) {
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the Primary Key for our Product
+            String id = databaseInfo.push().getKey();
+
+            //creating an Object
+            ServiceProviderInfo info = new ServiceProviderInfo(id,address,phoneNum,nameOfCompany,generalInfo,licensed);
+
+            //Saving the Product
+            databaseInfo.child(id).setValue(info);
+
+            //setting edittext to blank again
+            editAddress.setText("");
+            editPhoneNum.setText("");
+            editNameOfCompany.setText("");
+            editGeneralInfo.setText("");
+            editLicensed.setText("");
+
+
+            //displaying a success toast
+            Toast.makeText(this, "info added", Toast.LENGTH_LONG).show();
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a necessary text(s)", Toast.LENGTH_LONG).show();
+        }
+    }
 }
