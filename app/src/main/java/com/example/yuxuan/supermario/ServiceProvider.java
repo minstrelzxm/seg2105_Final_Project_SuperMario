2eda4bd7351aaa704b3752b4b3954496d60cf3e7
MaiCyclone <jyx940709@gmail.com>
2018-11-17 19:27:54 -0500

    ServiceProvider Class added

--- app/src/main/java/com/example/yuxuan/supermario/ServiceProvider.java
+++ app/src/main/java/com/example/yuxuan/supermario/ServiceProvider.java
@@ -1,7 +1,44 @@
 package com.example.yuxuan.supermario;
 
 public class ServiceProvider {
-    public ServiceProvider(){
 
+    private String address;
+    private String phoneNumber;
+    private String companyName;
+    private String generalDescription;
+    private String licensed;
+
+
+
+    public ServiceProvider(String address,String phoneNumber,String companyName){
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
+        this.generalDescription = "";
+        this.licensed = "NO";
+    }
+
+    public ServiceProvider(String address,String phoneNumber,String companyName,String generalDescription){
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
+        this.generalDescription = generalDescription;
+        this.licensed = "NO";
+    }
+
+    public ServiceProvider(String address,String phoneNumber,String companyName,String generalDescription,String licensed){
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
+        this.generalDescription = generalDescription;
+        this.licensed = licensed;
+    }
+
+    public void setGeneralDescription(String generalDescription){
+        this.generalDescription = generalDescription;
+    }
+
+    public void setLicensed(String licensed){
+        this.licensed = licensed;
     }
 }
