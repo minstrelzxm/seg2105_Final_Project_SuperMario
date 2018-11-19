6f3c1ac9c1031b490257ebfb97e25794c977885b
Xumou Zhang <jzhan279@uottawa.ca>
2018-11-18 23:25:27 -0500

    add update info page
    add update info page to service provider.

---
+++ app/src/main/java/com/example/yuxuan/supermario/service_provider_updated_infopage.java
@@ -0,0 +1,13 @@
+package com.example.yuxuan.supermario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class service_provider_updated_infopage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_provider_updated_infopage);
+    }
+}
