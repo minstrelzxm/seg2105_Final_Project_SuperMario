5bf5dbd1415d125767382b70e2ca4a0f43596da4
Xumou Zhang <jzhan279@uottawa.ca>
2018-11-19 02:24:58 -0500

    firebase new way save
    firebase new way save, we can use normal string as the id of group data

--- app/src/main/java/com/example/yuxuan/supermario/service_provider_updated_infopage.java
+++ app/src/main/java/com/example/yuxuan/supermario/service_provider_updated_infopage.java
@@ -1,13 +1,5 @@
 package com.example.yuxuan.supermario;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+public class service_provider_updated_infopage {
 
-public class service_provider_updated_infopage extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_provider_updated_infopage);
-    }
 }
