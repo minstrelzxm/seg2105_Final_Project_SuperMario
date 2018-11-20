c2a0af03c0989665e8f0362e6c60da35ae2c8103
dengmingwei <mingweideng@gmail.com>
2018-11-20 18:41:45 -0500

    TimeSlot display

--- app/src/main/java/com/example/yuxuan/supermario/ServiceProviderMainPage.java
+++ app/src/main/java/com/example/yuxuan/supermario/ServiceProviderMainPage.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceProviderMainPage extends AppCompatActivity {
 
@@ -12,6 +18,10 @@ public class ServiceProviderMainPage extends AppCompatActivity {
     private Button spAddServiceBtn;
     private Button spAvailabilitiesBtn;
     String username;
+    ArrayAdapter<String> adapter;
+    ArrayList myList = new ArrayList();
+    ArrayList<String> timeList = new ArrayList<>();
+    String day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +30,18 @@ public class ServiceProviderMainPage extends AppCompatActivity {
         spInfoBtn = (Button)findViewById(R.id.serviceProviderMainInfoBtn);
         spAddServiceBtn = (Button)findViewById(R.id.serviceProviderMainAddServiceBtn);
         spAvailabilitiesBtn = (Button)findViewById(R.id.serviceProviderMainAvailabtn);
+        ListView timeslotlistview = (ListView) findViewById(R.id.serviceProviderTimeSlot);
 
         Intent intent = getIntent();
+        Bundle args = intent.getBundleExtra("BUNDLE");
+
+        if (args != null) {
+            timeList = (ArrayList<String>) args.getSerializable("ARRAYLIST");
+            day = (String)args.getSerializable("Day");
+            myList.add(day);
+            myList.addAll(timeList);
+
+        }
         username = intent.getStringExtra("username");
 
         spInfoBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +64,16 @@ public class ServiceProviderMainPage extends AppCompatActivity {
                 availJump();
             }
         });
+
+        if (timeList != null) {
+            adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    myList);
+//        setListAdapter(adapter);
+            System.out.println("=========" + (timeslotlistview == null));
+            timeslotlistview.setAdapter(adapter);
+        }
+
     }
 
     public void infoJump(){
