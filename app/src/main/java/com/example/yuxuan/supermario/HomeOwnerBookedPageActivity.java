0789cbff3ccf4f4a18b144bb31eac87617a86aa3
MaiCyclone <jyx940709@gmail.com>
2018-12-05 09:42:47 -0500

    Booked Service xml

---
+++ app/src/main/java/com/example/yuxuan/supermario/HomeOwnerBookedPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.yuxuan.supermario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class HomeOwnerBookedPageActivity extends AppCompatActivity {
+    
+    ListView homeOwnerBookedListview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_owner_booked_page);
+    }
+}
